9a3fa98b5cf722bfc5ef26303e8ecf732b5b41c2
Eric <htx857@mocs.utc.edu>
2018-02-06 00:25:35 -0500

    Created base classes and methods

--- AutoAttendance/MainWindow.xaml.cs
+++ AutoAttendance/MainWindow.xaml.cs
@@ -24,5 +24,101 @@ namespace AutoAttendance
         {
             InitializeComponent();
         }
+
     }
+
+    public class Class
+    {
+        String className, classSemester;
+        int classSize;
+        
+        public void AddStudent()
+        {
+            //todo
+        }
+
+        public void RemoveStudent()
+        {
+            //todo
+        }
+
+        public void LoadClass()
+        {
+            //todo
+        }
+
+        public void SaveClass()
+        {
+            //todo
+        }
+
+        public void EditClass()
+        {
+            // todo
+        }
+    }
+
+    public class Student
+    { 
+        String sutdentName, year;
+        int[] attedance;
+        Image profile;
+        Image[] pictureList;
+
+        public void AddImage()
+        {
+            // todo
+        }
+
+        public void RemoveImage()
+        {
+            // todo
+        }
+
+        public void SetProfile()
+        {
+            // todo
+        }
+
+    }
+
+    public class Date
+    {
+        String Day;
+        int[] Attednance;
+        Image[] today;
+
+        public void UploadImage()
+        {
+            //todo
+        }
+
+        public void DeleteImage()
+        {
+            //todo
+        }
+
+    }
+
+    public class HandleImages
+    {
+        public void UploadImage()
+        {
+            // todo
+        }
+
+        public void GetImage()
+        {
+            // todo
+        }
+
+        public void DeleteImage()
+        {
+            // todo
+        }
+
+    }
+
+
+
 }
